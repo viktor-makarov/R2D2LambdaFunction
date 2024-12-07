e5bb3663344794115a680b87669f7ce3844e1705
Макаров Виктор Александрович <makarov.v@prs-va.ru>
2024-12-07 23:05:06 +0300

    before test in linux

--- python/countTokens/main.py
+++ python/countTokens/main.py
@@ -4,10 +4,16 @@ import json
 from datetime import datetime
 import time
 
-sys.path.insert(0, os.path.abspath(r'..\\lib\\python3.10\\site-packages'))
+
+sys.path = []
+dirname = os.path.dirname(os.path.dirname(__file__))
+libpath = os.path.join(dirname, 'lib/python3.10/site-packages')
+for path in [libpath,'C:\\Program Files\\WindowsApps\\PythonSoftwareFoundation.Python.3.10_3.10.3056.0_x64__qbz5n2kfra8p0\\DLLs', 'C:\\Program Files\\WindowsApps\\PythonSoftwareFoundation.Python.3.10_3.10.3056.0_x64__qbz5n2kfra8p0\\lib','/var/task', '/opt/python/lib/python3.10/site-packages', '/opt/python', '/var/runtime', '/var/lang/lib/python310.zip', '/var/lang/lib/python3.10', '/var/lang/lib/python3.10/lib-dynload', '/var/lang/lib/python3.10/site-packages']:
+    sys.path.insert(0, path)
+
+#print("sys.path inside main",sys.path)
 
 import tiktoken
-
 
 def num_tokens_from_text(text, model="gpt-4o"):
 
@@ -63,10 +69,8 @@ def num_tokens_from_message(text, model="gpt-4o"):
 
 def convertToObject(value):
     if isinstance(value, dict):
-        print("dict")
         return value
     elif isinstance(value, str):
-        print("str")
         return json.loads(value)
     else:
         raise NotImplementedError(
