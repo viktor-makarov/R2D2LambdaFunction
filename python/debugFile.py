7d35534af963281b0d9e883dbadda3e492c078d8
EC2 Default User <ec2-user@ip-172-31-42-148.eu-north-1.compute.internal>
2024-12-11 22:04:45 +0000

    wo OCR

--- python/debugFile.py
+++ python/debugFile.py
@@ -1,6 +1,15 @@
 import extractTextFromFile;
 
-event = {"url": "https://r2d2storagedev.s3.eu-north-1.amazonaws.com/incoming_files/test_text2.txt"}
+
+event = {"file_url":"https://r2d2storagedev.s3.eu-north-1.amazonaws.com/incoming_files/5248593849_1893.pdf","file_mime_type":"application/pdf"}
+
+#event = {"file_url": "https://r2d2storagedev.s3.eu-north-1.amazonaws.com/incoming_files/5248593849_1866.txt", "file_mime_type": "text/plain"}
+
+#event = {"file_url":"https://r2d2storagedev.s3.eu-north-1.amazonaws.com/incoming_files/5248593849_1881.xlsx","file_mime_type":"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"}
+
+#event = {"file_url": "https://r2d2storagedev.s3.eu-north-1.amazonaws.com/incoming_files/5248593849_1867.json", "file_mime_type": "application/json"}
+
+#event = {"file_url":"https://r2d2storagedev.s3.eu-north-1.amazonaws.com/incoming_files/5248593849_1875.docx","file_mime_type":"application/vnd.openxmlformats-officedocument.wordprocessingml.document"}
 
 result = extractTextFromFile.extractTextFromFileRouter(event,"")
 
--- python/extractTextFromFile/main.py
+++ python/extractTextFromFile/main.py
@@ -1,10 +1,10 @@
 import http.client
 import urllib.parse
 from io import BytesIO
-import filetype
 from docx import Document
 import openpyxl
 import PyPDF2
+
 
 def download_file(url):
     # Parse the URL to extract components
@@ -15,18 +15,12 @@ def download_file(url):
     
     if response.status != 200:
         raise Exception(f"Failed to download file: {url} {response.status} {response.reason}")
+
+    binaryObject = BytesIO(response.read())
+    binaryObject.name = url_parts.path.split('/')[-1]
     
     # Read the response data into a BytesIO object
-    return BytesIO(response.read())
-
-def getmimetype(byte_stream):
-    # Move to the beginning of the stream
-    byte_stream.seek(0)
-    # Use filetype to detect MIME type
-    kind = filetype.guess(byte_stream.read(2048))  # Read the first 2048 bytes for better detection
-    if kind is None:
-        raise ValueError("Could not determine MIME type")
-    return kind.mime
+    return binaryObject
 
 def extracttextfromtxt(txt_stream):
     return txt_stream.read().decode('utf-8')
@@ -44,28 +38,56 @@ def extracttextfromdocx(docx_stream):
     doc = Document(docx_stream)
     return "\n".join([paragraph.text for paragraph in doc.paragraphs])
 
+def extracttextfromxlsx(xlsx_stream):
+    wb = openpyxl.load_workbook(xlsx_stream, data_only=True)
+    text = []
+    for sheet in wb.sheetnames:
+        ws = wb[sheet]
+        for row in ws.iter_rows():
+            text.append(' '.join([str(cell.value) if cell.value is not None else '' for cell in row]))
+    return "\n".join(text)
+
 def extracttextfrompdf(pdf_stream):
-    text = ""
+
+    text = ''
     pdf_reader = PyPDF2.PdfReader(pdf_stream)
     for page in pdf_reader.pages:
         text += page.extract_text()
     return text
 
+def extracttextfromtxt(filestream):
+    return filestream.read().decode('utf-8')
+
+def extracttextfromdocx(docx_stream):
+    doc = Document(docx_stream)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
+
+    doc = Document(docx_stream)
+    return "\n".join([paragraph.text for paragraph in doc.paragraphs])
+
+def ocrPDF(filestream):
+    return "OCR yе реализован"    
+
 def extracttextfromfile(filestream, mime_type):
-    text = ""
-    
+
     if mime_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-        print("doc")
         text = extracttextfromdocx(filestream)
     elif mime_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        print("Excel")
         text = extracttextfromxlsx(filestream)
-    elif mime_type.startswith('text/'):        
-        print("Text")
+    elif mime_type.startswith('text/') or mime_type == 'application/json':
         text = extracttextfromtxt(filestream)
     elif mime_type == 'application/pdf':
-        print("PDF")
         text = extracttextfrompdf(filestream)
+        noTextFound = len(text) < 10
+
+        if noTextFound:
+            text = ocrPDF(filestream)
+        
+    elif mime_type == 'image/jpeg':
+        text = ocrImage(filestream)
     else:
         raise ValueError("Unsupported file type")
     
@@ -73,17 +95,26 @@ def extracttextfromfile(filestream, mime_type):
 
 def extractTextFromFileRouter(event, context):
   
-    url = event["url"]
-    filestream = download_file(url)
-    mime_type  = getmimetype(filestream)
-    print("mimetype",mime_type)
-
-    if mime_type is None:
-        raise ValueError("Could not determine MIME type")
+    url = event["file_url"]
     
-    text = extracttextfromfile(filestream, mime_type)
+    try:
+        filestream = download_file(url)
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': {"error":'Could not fetch the file by url provided',"success":0}
+        }
+    mime_type  = event["file_mime_type"]
+
+    try:
+        text = extracttextfromfile(filestream, mime_type)
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': {"error":e,"success":0}
+        }
 
     return {
         'statusCode': 200,
-        'body': {text:text}
+        'body': {"text":text,"success":1}
     }
